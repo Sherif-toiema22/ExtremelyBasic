467300f43098cce8bc4d2671dad4de6c20212322
sherif abdelkader <sherifabdelkaderahmedabdelaziz@gmail.com>
2023-04-06 22:38:29 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner= new Scanner(System.in);
+        int input1= scanner.nextInt();
+        int input2= scanner.nextInt();
+        int result=input1+input2;
+        System.out.println("X = "+result);
+
+
+	// write your code here
+    }
+}
